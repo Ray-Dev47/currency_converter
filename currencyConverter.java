48f4c666f122496dfc09476fff8bd9bec1e1f080
Raymond <raymond.idengeli@gmail.com>
2023-10-22 14:21:56 +0100

    method two completed

--- currencyConverter.java
+++ currencyConverter.java
@@ -1,71 +1,79 @@
-import java.util.Scanner;
+import java. util.Scanner;
 
-/**
- * currencyConverter
- * 
-First we are going to take the user currency input in which currency he wants to enter the amount
-Then will take the amount of input from the user
-Then using the switch case and function calculate the amount in the destinated currency
- */
 public class currencyConverter {
 
     public static void main(String[] args) {
+        System.out.println("1 Ruppe");
+        System.out.println("2 Dollar");
+        System.out.println("3 Euro");
+        // take input
         Scanner sc = new Scanner(System.in);
-        System.out.println("-----------------");
-        System.out.println("Please select select currencr below");
-        System.out.println("1. Dollar");
-        System.out.println("2. Naira");
-        System.out.println("3. Pounds");
-        System.out.println("4. Euro");
-        System.out.println("-----------------");
-        int num = sc.nextInt();
+// 		take currency as input
+        System.out.println("Choose the currency");
 
-        // int amount;
-          System.out.println("Enter the amount");
-         int amount = sc.nextInt();
-        
-
-        switch (num) {
-            case 1:
-               double convertedNaira = amount * 1100;
-               double convertedPounds = amount *  0.822978;
-               double convertedEuro = amount *  0.94355;
-               System.out.println(amount + " Dollars is equal to " + convertedNaira + " Naira" );
-               System.out.println(amount + " Dollars is equal to " + convertedPounds + " Pounds" );
-               System.out.println(amount + " Dollars is equal to " + convertedEuro + " Euro" );
-               break;
-            
-            case 2:
-              double convertedDollar = amount * 1100;
-               double convertedPounds1 = amount *  0.822978;
-               double convertedEuro1 = amount *  0.94355;
-               System.out.println(amount + " Naira is equal to " + convertedDollar + " Dollars" );
-               System.out.println(amount + " Naira is equal to " + convertedPounds1 + " Pounds" );
-               System.out.println(amount + " Naira is equal to " + convertedEuro1 + " Euro" );
-               break;
-
-            case 3:
-              double convertedDollar2 = amount * 1.2151;
-               double convertedNaira2 = amount *  1500;
-               double convertedEuro2 = amount * 1.146508;
-               System.out.println(amount + " Pounds is equal to " + convertedDollar2 + " Dollars" );
-               System.out.println(amount + " Pounds is equal to " + convertedNaira2 + " Naira" );
-               System.out.println(amount + " Pounds is equal to " + convertedEuro2 + " Euro" );
-               break;
-
-            case 4:
-              double convertedDollar3 = amount * 1.060354;
-               double convertedNaira3 = amount *  810.325377;
-               double convertedPounds3 = amount *  0.872214;
-               System.out.println(amount + " Euro is equal to " + convertedDollar3 + " Dollars" );
-               System.out.println(amount + " Euro is equal to " + convertedNaira3 + " Naira" );
-               System.out.println(amount + " Euro is equal to " + convertedPounds3 + " Euro" );
-               break;
-
-        
-            default:
-                break;
-        }
+        int choice = sc.nextInt();
+        System.out.println("Enter the amount");
+        double amount = sc.nextDouble();
         sc.close();
+        // convert the amount
+        switch (choice) {
+            case 1:
+                Ruppe_to_other(amount);
+                break;
+            case 2:
+                Dollar_to_other(amount);
+                break;
+            case 3:
+                Euro_to_other(amount);
+                break;
+            default:
+                System. out.println("Invalid choice");
+        }
+
+    
     }
+
+    
+
+
+public static void Ruppe_to_other(double amt) {
+    System.out.println("1 Ruppe = " + 0.013 + " Dollar");
+    System.out.println();
+
+    System.out.println(amt+" Ruppe = " + (amt*0.013) + " Dollar");
+    System.out.println();
+
+    System.out.println("1 Ruppe = " + 0.012 + " Euro");
+    System.out.println();
+    System.out.println(amt+" Ruppe = " + (amt*0.012) + " Euro");
+    System.out.println();
+
+}
+    
+
+public static void Dollar_to_other(double amt) {
+    System.out.println("1 Dollar = " + 79.37 + " Ruppe");
+    System.out.println();
+    System.out.println(amt+" Dollar = " + (amt*79.37) + " Ruppe");
+    System.out.println();
+
+    System.out.println("1 Dollar= " + 0.98 + " Euro");
+    System.out.println();
+
+    System.out.println(amt+" Dollar = " + (amt*0.98) + " Euro");
+}
+
+    
+
+public static void Euro_to_other(double amt){
+    System.out.println("1 Euro = " + 80.85 + " Ruppe");
+    System.out.println();
+    System.out.println(amt+" Euro = " + (amt*80.85) + " Ruppe");
+    System.out.println();
+
+    System.out.println("1 Euro = " + 1.02 + " Dollar");
+    System.out.println();
+
+    System.out.println(amt+" Euro = " + (amt*1.02) + " Dollar");
+}
 }
